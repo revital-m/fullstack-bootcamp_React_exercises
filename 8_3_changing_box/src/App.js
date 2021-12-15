df3dcb36f809d7c4ca6a943bbb2ec0f5fc7174a9
revital-m <revital13m@gmail.com>
2021-12-15 13:37:06 +0200

    ex 8.3 - done

---
+++ 8_3_changing_box/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Box from "./components/Box";
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="box-container">
+        <Box />      
+      </div>
+    );
+  }
+}
+
+export default App;
