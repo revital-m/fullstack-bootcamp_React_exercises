ba3bf19eb6d7310a30796fab1b928ec71a894dde
revital-m <revital13m@gmail.com>
2021-12-29 09:16:01 +0200

    ex 21.2

--- 21_2_play_pause_video/src/App.js
+++ 21_2_play_pause_video/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         ref={myVideo}
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/fUXdrl9ch_Q"
+        src="/"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
