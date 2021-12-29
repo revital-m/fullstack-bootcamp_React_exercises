f1ee791eb1ad76679ea20a87fe715b33e9eb9a76
revital-m <revital13m@gmail.com>
2021-12-29 09:11:52 +0200

    ex 21.3 - done

--- 21_3_image_toggle/src/App.js
+++ 21_3_image_toggle/src/App.js
@@ -2,13 +2,13 @@ import React, { useRef } from "react";
 import "./App.css";
 
 const App = () => {
-  const myImg = useRef(null);
+  const myImg1 = useRef(null);
   const myImg2 = useRef(null);
 
-  function handleBlackColor(e) {
+  function handleBlackAndWhiteImg(e) {
     switch (e.target.name) {
       case "img1":
-        myImg.current.style.filter = "grayscale(0%)";
+        myImg1.current.style.filter = "grayscale(0%)";
         break;
       case "img2":
         myImg2.current.style.filter = "grayscale(0%)";
@@ -16,10 +16,10 @@ const App = () => {
     }
   }
 
-  function handleColors(e) {
+  function handleColorsImg(e) {
     switch (e.target.name) {
       case "img1":
-        myImg.current.style.filter = "grayscale(100%)";
+        myImg1.current.style.filter = "grayscale(100%)";
         break;
       case "img2":
         myImg2.current.style.filter = "grayscale(100%)";
@@ -30,18 +30,20 @@ const App = () => {
   return (
     <div className="container">
       <img
-        ref={myImg}
+        ref={myImg1}
         name="img1"
-        onMouseOver={handleBlackColor}
-        onMouseOut={handleColors}
-        src="https://images.unsplash.com/photo-1640622209668-7a267d57766b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        onMouseOver={handleBlackAndWhiteImg}
+        onMouseOut={handleColorsImg}
+        src="https://images.unsplash.com/photo-1459478309853-2c33a60058e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        className="image"
       />
       <img
         ref={myImg2}
         name="img2"
-        onMouseOver={handleBlackColor}
-        onMouseOut={handleColors}
-        src="https://images.unsplash.com/photo-1640616516113-baf892a564e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
+        onMouseOver={handleBlackAndWhiteImg}
+        onMouseOut={handleColorsImg}
+        src="https://images.unsplash.com/photo-1558470598-a5dda9640f68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+        className="image"
       />
     </div>
   );
